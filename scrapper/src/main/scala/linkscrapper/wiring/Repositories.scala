f54d9a6c92ea700b5a0ecadc15290b7f34e99b76
artrsyf <rom.fadeev2017@yandex.ru>
2025-03-05 22:19:54 +0300

    Update wiring with link repo & usecase

--- scrapper/src/main/scala/linkscrapper/wiring/Repositories.scala
+++ scrapper/src/main/scala/linkscrapper/wiring/Repositories.scala
@@ -1,17 +1,28 @@
 package linkscrapper.wiring
 
-import linkscrapper.Chat.domain.model
+import linkscrapper.Chat.domain.model.Chat
+import linkscrapper.Link.domain.model.Link
 import linkscrapper.Chat.repository.ChatRepository
+import linkscrapper.Link.repository.LinkRepository
 import linkscrapper.Chat.repository.InMemory.InMemoryChatRepository
+import linkscrapper.Link.repository.InMemory.InMemoryLinkRepository
+
 import cats.effect.{IO, Ref}
 
 final case class Repositories(
-  chatRepo: ChatRepository[IO]
+  chatRepo: ChatRepository[IO],
+  linkRepo: LinkRepository[IO],
 )
 
 object Repositories:
   def make: IO[Repositories] =
     for 
-        chatData <- Ref.of[IO, Map[Long, model.Chat]](Map.empty)
+        chatData <- Ref.of[IO, Map[Long, Chat]](Map.empty)
+        linkData <- Ref.of[IO, Map[String, Link]](Map.empty)
+
         chatRepo = InMemoryChatRepository(chatData)
-    yield Repositories(chatRepo)
+        linkRepo = InMemoryLinkRepository(linkData)
+    yield Repositories(
+      chatRepo,
+      linkRepo,
+    )
--- scrapper/src/main/scala/linkscrapper/wiring/Usecases.scala
+++ scrapper/src/main/scala/linkscrapper/wiring/Usecases.scala
@@ -1,15 +1,23 @@
 package linkscrapper.wiring
 
-import linkscrapper.Chat.usecase.ChatUsecase
 import linkscrapper.Chat.repository.ChatRepository
+import linkscrapper.Link.repository.LinkRepository
+import linkscrapper.Chat.usecase.ChatUsecase
+import linkscrapper.Link.usecase.LinkUsecase
+
 import cats.effect.IO
 
 final case class Usecases(
-  chatUsecase: ChatUsecase[IO]
+  chatUsecase: ChatUsecase[IO],
+  linkUsecase: LinkUsecase[IO],
 )
 
 object Usecases:
   def make(repos: Repositories): Usecases =
     val chatUsecase  = ChatUsecase.make(repos.chatRepo)
+    val linkUsecase = LinkUsecase.make(repos.linkRepo)
 
-    Usecases(chatUsecase)
+    Usecases(
+      chatUsecase,
+      linkUsecase,
+    )
