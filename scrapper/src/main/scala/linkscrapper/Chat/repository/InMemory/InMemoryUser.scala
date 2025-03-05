acca93d8d03bfc5fb7e5cc75ffacddd483d9f064
artrsyf <rom.fadeev2017@yandex.ru>
2025-03-05 20:38:58 +0300

    Create chat repository iface & in memory impl

---
+++ scrapper/src/main/scala/linkscrapper/Chat/repository/InMemory/InMemoryUser.scala
@@ -0,0 +1,27 @@
+package linkscrapper.Chat.repository.InMemory
+
+import linkscrapper.Chat.repository
+import linkscrapper.Chat.domain.model
+import linkscrapper.Chat.domain.entity
+import linkscrapper.Chat.domain.dto
+
+import cats.effect.Ref
+import cats.effect.IO
+
+final class InMemoryChatRepository(
+    data: Ref[IO, Map[Long, model.Chat]]
+) extends repository.ChatRepository[IO]:
+    override def create(chatEntity: entity.Chat): IO[model.Chat] = 
+        IO.println(s"Successfully created chat with id: ${chatEntity.ChatId}")
+
+        for 
+            chatModel <- IO.pure(
+                dto.ChatEntityToModel(chatEntity)
+            )
+            _ <- data.update(_ + (chatModel.ChatId -> chatModel))
+        yield chatModel
+
+    override def delete(chatId: Long): IO[Unit] = 
+        IO.println(s"Successfully deleted chat with id: $chatId")
+
+        data.update(_.removed(chatId))
---
+++ scrapper/src/main/scala/linkscrapper/Chat/repository/repository.scala
@@ -0,0 +1,8 @@
+package linkscrapper.Chat.repository
+
+import linkscrapper.Chat.domain.entity
+import linkscrapper.Chat.domain.model
+
+trait ChatRepository[F[_]]:
+    def create(chatEntity: entity.Chat): F[model.Chat]
+    def delete(chatId: Long): F[Unit]
