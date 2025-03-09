9a63869a8841aa81c0f3097230229364e81ee548
artrsyf <rom.fadeev2017@yandex.ru>
2025-03-09 09:29:51 +0300

    Update GitHub client as class

--- scrapper/src/main/scala/linkscrapper/pkg/Client/GitHubClient/GitHubClient.scala
+++ scrapper/src/main/scala/linkscrapper/pkg/Client/GitHubClient/GitHubClient.scala
@@ -17,25 +17,51 @@ import tethys.jackson._
 
 final case class GitHubRepo(updated_at: String) derives JsonReader
 
+trait GitHubClient[F[_]]:
+  def getRepoUpdate(owner: String, repo: String): F[Either[String, GitHubRepo]]
+
 object GitHubClient:
-  def getRepoUpdate[F[_]: Async](client: SttpBackend[F, Any], owner: String, repo: String): F[Either[String, GitHubRepo]] =
-    val request = basicRequest
-      .get(uri"https://api.github.com/repos/$owner/$repo")
+  final private class Impl(
+    client: SttpBackend[IO, Any],
+  ) extends GitHubClient[IO]:
+    override def getRepoUpdate(owner: String, repo: String): IO[Either[String, GitHubRepo]] = 
+      val request = basicRequest
+        .get(uri"https://api.github.com/repos/$owner/$repo")
     
-    client.send(request).fmap { response =>
-      response.body match
-        case Right(json) =>
-          json.jsonAs[GitHubRepo] match
-            case Right(repo) => Right(repo)
-            case Left(err)   => Left(s"JSON decode error: $err")
+      client.send(request).fmap { response =>
+        response.body match
+          case Right(json) =>
+            json.jsonAs[GitHubRepo] match
+              case Right(repo) => Right(repo)
+              case Left(err)   => Left(s"JSON decode error: $err")
+          
+          case Left(error) =>
+              Left(s"HTTP error: $error")
+      }
+  
+  def make(client: SttpBackend[IO, Any]): GitHubClient[IO] = 
+    Impl(client)
+
+// object GitHubClient:
+//   def getRepoUpdate[F[_]: Async](client: SttpBackend[F, Any], owner: String, repo: String): F[Either[String, GitHubRepo]] =
+//     val request = basicRequest
+//       .get(uri"https://api.github.com/repos/$owner/$repo")
+    
+//     client.send(request).fmap { response =>
+//       response.body match
+//         case Right(json) =>
+//           json.jsonAs[GitHubRepo] match
+//             case Right(repo) => Right(repo)
+//             case Left(err)   => Left(s"JSON decode error: $err")
         
-        case Left(error) =>
-            Left(s"HTTP error: $error")
-    }
+//         case Left(error) =>
+//             Left(s"HTTP error: $error")
+//     }
 
 @main def run(): Unit =
   HttpClientCatsBackend.resource[IO]().use { backend =>
-    GitHubClient.getRepoUpdate(backend, "typelevel", "cats-effect")
+    val ghClient = GitHubClient.make(backend)
+    ghClient.getRepoUpdate("typelevel", "cats-effect")
       .flatMap {
         case Right(repo) =>
           IO.println(s"Last update: ${repo.updated_at}")
