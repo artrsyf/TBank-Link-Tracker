4e8e44b9803a41944f4002232045a0a910bd7690
artrsyf <rom.fadeev2017@yandex.ru>
2025-06-14 18:14:23 +0300

    Implement kafka producer for scrapper service

--- scrapper/src/main/scala/linkscrapper/Main.scala
+++ scrapper/src/main/scala/linkscrapper/Main.scala
@@ -1,6 +1,6 @@
 package linkscrapper
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{ExitCode, IO, IOApp, Resource}
 import doobie.hikari.HikariTransactor
 import com.comcast.ip4s.{Host, Port}
 import com.zaxxer.hikari.HikariConfig
@@ -11,6 +11,7 @@ import org.typelevel.log4cats.Logger
 import sttp.client3.httpclient.cats.HttpClientCatsBackend
 import sttp.tapir.server.http4s.Http4sServerInterpreter
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
+import fs2.kafka._
 
 import linkscrapper.chat.delivery.http.ChatHandler
 import linkscrapper.config.AppConfig
@@ -21,101 +22,133 @@ import linkscrapper.pkg.Client.StackOverflowClient.StackOverflowClient
 import linkscrapper.pkg.Scheduler.QuartzScheduler
 import linkscrapper.wiring.{Repositories, Usecases}
 import linkscrapper.pkg.Publisher.HttpPublisher.HttpPublisher
+import linkscrapper.pkg.Publisher.KafkaPublisher.KafkaLinkPublisher
+import linkscrapper.config.KafkaConfig
 
 object Main extends IOApp:
   given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
-  private def getPortSafe(v: Int): IO[Port] =
-    IO.delay(
-      Port.fromInt(v).toRight(IllegalArgumentException("Invalid port value"))
-    ).rethrow
-
   override def run(args: List[String]): IO[ExitCode] =
     for {
       appConfig <- AppConfig.load
+      hikariConfig = makeHikariConfig(appConfig)
+      hikariResource = createHikariTransactorResource(hikariConfig).map(Some(_))
 
-      hikariConfig = {
-        val config = new HikariConfig()
-        config.setDriverClassName(appConfig.db.driver)
-        config.setJdbcUrl(appConfig.db.url)
-        config.setUsername(appConfig.db.user)
-        config.setPassword(appConfig.db.password)
-        config
-      }
-
-      hikariTransactorResource = HikariTransactor.fromHikariConfig[IO](hikariConfig)
-
-      exitCode <- {
-        if appConfig.db.inUse == "postgres" then
-          hikariTransactorResource.use { transactor =>
-            runApp(Some(transactor), appConfig)
-          }
-        else
-          runApp(None, appConfig) // in-memory
-      }
+      exitCode <- appConfig.db.inUse match
+        case "postgres" => hikariResource.use(runApp(_, appConfig))
+        case _          => runApp(None, appConfig)
     } yield exitCode
 
-  def runApp(
+  private def makeHikariConfig(appConfig: AppConfig): HikariConfig =
+    val config = new HikariConfig()
+    config.setDriverClassName(appConfig.db.driver)
+    config.setJdbcUrl(appConfig.db.url)
+    config.setUsername(appConfig.db.user)
+    config.setPassword(appConfig.db.password)
+    config
+
+  private def getPortSafe(v: Int): IO[Port] =
+    IO.delay(Port.fromInt(v).toRight(new IllegalArgumentException("Invalid port value"))).rethrow
+
+  private def createKafkaProducerResource(config: KafkaConfig): Resource[IO, KafkaProducer[IO, String, String]] =
+    KafkaProducer.resource(ProducerSettings[IO, String, String].withProperties(config.properties))
+
+  private def createHikariTransactorResource(config: HikariConfig): Resource[IO, HikariTransactor[IO]] =
+    HikariTransactor.fromHikariConfig[IO](config)
+
+  private def resources(
+    hikariTransactor: Option[HikariTransactor[IO]],
+    appConfig: AppConfig
+  ): Resource[IO, (
+    HikariTransactor[IO],
+    LinkClient[IO],
+    LinkClient[IO],
+    sttp.client3.SttpBackend[IO, _],
+    linkscrapper.pkg.Publisher.LinkPublisher[IO]
+  )] = for {
+    backend <- HttpClientCatsBackend.resource[IO]()
+
+    githubClient        = GitHubClient.make(backend)
+    stackoverflowClient = StackOverflowClient.make(backend)
+
+    linkPublisher <- appConfig.transport.`type` match
+      case "kafka" =>
+        createKafkaProducerResource(appConfig.transport.kafka).map { producer =>
+          new KafkaLinkPublisher(
+            appConfig.transport.kafka.topic,
+            producer,
+            logger
+          )
+        }
+      case "http" =>
+        Resource.pure[IO, linkscrapper.pkg.Publisher.LinkPublisher[IO]](
+          HttpPublisher(appConfig.transport.http.updatesHandlerEndpoint, backend, logger)
+        )
+      case other =>
+        Resource.eval(IO.raiseError(new RuntimeException(s"Unsupported transport type: $other")))
+
+  } yield (
+    hikariTransactor.getOrElse(throw new IllegalStateException("Missing transactor")),
+    githubClient,
+    stackoverflowClient,
+    backend,
+    linkPublisher
+  )
+
+  private def runApp(
       hikariTransactor: Option[HikariTransactor[IO]],
       appConfig: AppConfig
   ): IO[ExitCode] =
-    for {
-      backend             <- HttpClientCatsBackend.resource[IO]().use(IO.pure)
-      githubClient        <- IO(GitHubClient.make(backend))
-      stackoverflowClient <- IO(StackOverflowClient.make(backend))
+    resources(hikariTransactor, appConfig).use {
+      case (transactor, githubClient, stackoverflowClient, backend, linkPublisher) =>
 
-      clients = Map[String, LinkClient[IO]](
-        "https://github.com/"        -> githubClient,
-        "https://stackoverflow.com/" -> stackoverflowClient,
-      )
+        val clients = Map(
+          "https://github.com/"        -> githubClient,
+          "https://stackoverflow.com/" -> stackoverflowClient,
+        )
 
-      repositories <- Repositories.make(appConfig.db.inUse, hikariTransactor)
-      usecases = Usecases.make(repositories, clients.keys.toList)
-      linkPublisher = HttpPublisher(
-        appConfig.scheduler.updatesHandlerUrl, 
-        backend, 
-        logger
-      )
+        for {
+          repositories <- Repositories.make(appConfig.db.inUse, Some(transactor))
+          usecases = Usecases.make(repositories, clients.keys.toList)
 
-      scheduler = QuartzScheduler(
-        appConfig.scheduler,
-        usecases.linkUsecase,
-        linkPublisher,
-        clients,
-        logger,
-      )
-
-      endpoints <- IO {
-        List(
-          ChatHandler.make(usecases.chatUsecase),
-          LinkHandler.make(usecases.chatUsecase, usecases.linkUsecase, logger),
-        ).flatMap(_.endpoints)
-      }
-
-      swaggerEndpoint = SwaggerInterpreter().fromServerEndpoints[IO](
-        endpoints,
-        "Scrapper API",
-        "1.0.0"
-      )
-
-      routes = Http4sServerInterpreter[IO]()
-        .toRoutes(swaggerEndpoint ++ endpoints)
-
-      port <- getPortSafe(appConfig.server.port)
-
-      _ <- IO.race(
-        EmberServerBuilder
-          .default[IO]
-          .withHost(Host.fromString("0.0.0.0").get)
-          .withPort(port)
-          .withHttpApp(Router("/" -> routes).orNotFound)
-          .build
-          .evalTap(server =>
-            Logger[IO].info(
-              s"Server available at http://0.0.0.0:${server.address.getPort}"
-            )
+          scheduler = QuartzScheduler(
+            appConfig.scheduler,
+            usecases.linkUsecase,
+            linkPublisher,
+            clients,
+            logger
           )
-          .useForever,
-        scheduler.runScheduler
-      )
-    } yield ExitCode.Success
+
+          endpoints <- IO {
+            List(
+              ChatHandler.make(usecases.chatUsecase),
+              LinkHandler.make(usecases.chatUsecase, usecases.linkUsecase, logger)
+            ).flatMap(_.endpoints)
+          }
+
+          swaggerEndpoint = SwaggerInterpreter().fromServerEndpoints[IO](
+            endpoints,
+            "Scrapper API",
+            "1.0.0"
+          )
+
+          port <- getPortSafe(appConfig.server.port)
+
+          routes = Http4sServerInterpreter[IO]()
+            .toRoutes(swaggerEndpoint ++ endpoints)
+
+          _ <- IO.race(
+            EmberServerBuilder
+              .default[IO]
+              .withHost(Host.fromString("0.0.0.0").get)
+              .withPort(port)
+              .withHttpApp(Router("/" -> routes).orNotFound)
+              .build
+              .evalTap(server =>
+                logger.info(s"Server available at http://0.0.0.0:${server.address.getPort}")
+              )
+              .useForever,
+            scheduler.runScheduler
+          )
+        } yield ExitCode.Success
+    }
---
+++ scrapper/src/main/scala/linkscrapper/pkg/Publisher/KafkaPublisher/KafkaPublisher.scala
@@ -0,0 +1,54 @@
+package linkscrapper.pkg.Publisher.KafkaPublisher
+
+import cats.effect.IO
+import cats.syntax.all._
+import fs2.kafka._
+import fs2.Stream
+import org.typelevel.log4cats.Logger
+import tethys._
+import tethys.jackson._
+
+import linkscrapper.pkg.Publisher.LinkPublisher
+import linkscrapper.link.domain.dto.LinkUpdate
+
+class KafkaLinkPublisher(
+    topic: String,
+    producer: KafkaProducer[IO, String, String],
+    logger: Logger[IO]
+) extends LinkPublisher[IO] {
+
+  override def publishLinks(linkUpdates: List[LinkUpdate]): IO[Unit] = {
+    if (linkUpdates.isEmpty) {
+      logger.info("Notjing to update, skipping")
+    } else {
+      val json = linkUpdates.asJson
+      val record = ProducerRecord(topic, "", json)
+      
+      for {
+        _ <- logger.info(s"Sending updates to kafka topic $topic | linkCount=${linkUpdates.length}")
+        
+        result <- producer.produceOne(record).flatten.attempt
+        
+        _ <- result match {
+          case Right(responseChunk) =>
+            Stream
+                .chunk(responseChunk)
+                .evalTap { case (_, metadata) =>
+                logger.info(
+                    s"Successfully published ${linkUpdates.length} updates to Kafka | " +
+                    s"topic=${metadata.topic()} | partition=${metadata.partition()} | offset=${metadata.offset()}"
+                )
+                }
+                .compile
+                .drain
+          
+          case Left(error) =>
+            logger.error(
+              s"Failed to publish ${linkUpdates.length} updates to Kafka | " +
+              s"error=${error.getMessage}"
+            )
+        }
+      } yield ()
+    }
+  }
+}
